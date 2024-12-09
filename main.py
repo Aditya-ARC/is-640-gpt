e43ee0dbeb96f078df1faa1f96d9f960245c876a
Aditya-ARC <adityachitnis@gmail.com>
2024-12-09 13:00:03 -0800

    main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import torch
 from data import Data
 from model import GPTLanguageModel
 from trainer import Trainer
-
+#definingmain
 if __name__ == "__main__":
     # Hyperparameters
-    batch_size = 16 # Increasing batch size for faster training
+     # Increasing batch size for faster training
+    batch_size = 16 
     block_size = 8
-    max_iters = 100    # For better performance increasing number of training iterations
+    # For better performance increasing number of training iterations
+    max_iters = 100    
     eval_interval = 500
     eval_iters = 200
     learning_rate = 1e-3  
@@ -17,10 +19,10 @@ if __name__ == "__main__":
     n_layer = 3
     dropout = 0.2
 
-    # Data Loading
+    #Inputfile
     data_obj = Data(file_path='input.txt', device=device, block_size=block_size)
 
-    # Model Creation 
+    #Creatingclassmodel
     model = GPTLanguageModel(
         vocab_size=data_obj.vocab_size,
         n_embd=n_embd, 
@@ -31,11 +33,12 @@ if __name__ == "__main__":
         device=device
     ).to(device)
 
-    # Model Training
+    #Training the model
     trainer = Trainer(model, data_obj, max_iters=max_iters, eval_interval=eval_interval, eval_iters=eval_iters, batch_size=batch_size, learning_rate=learning_rate)
     trainer.train()
 
-    # Text Generation
     context = torch.zeros((1, 1), dtype=torch.long, device=device)
     generated_ids = model.generate(context, max_new_tokens=100)
-    print(data_obj.decode(generated_ids[0].tolist()))
+    print(data_obj.decode(generated_ids[0].tolist())) 
+
+#cloning classes and training models
